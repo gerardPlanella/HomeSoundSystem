f731499a66a4ce95ed5dd0d79b07b190c47f9523
gerardPlanella <gerard.plfo@gmail.com>
2020-03-16 18:00:19 +0100

    core.py developed

--- Core/core.py
+++ Core/core.py
@@ -1,6 +1,6 @@
 import socket
-from _thread import *
 import threading
+import dedicatedServer
 
 ## TODO: Make Client class
 
@@ -8,14 +8,27 @@ HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 8000         # Port to listen on (non-privileged ports are > 1023)
 
 sensorNames = ['kitchen', 'living_room', 'bath_room', 'bed_room']
+sensorThreads = []
 
-def startThread():
-    x = threading.Thread(target = existenceReminder, args = (status, ))
-    x.start()
-    return x
+threadInfo = threadInfo()
+
+class threadInfo():
+    __slots__ = ["running"]
+
+    def __init__(self):
+        self.running = True
+
+    def setRun(self, running):
+        self.running = running
+    def getRun(self):
+        return self.running
 
 
 def main():
+
+    eventProcessor = threading.Thread(target = eventProcessing, args = (threadInfo, ))
+    eventProcessor.start()
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((HOST, PORT))
     sock.listen(5)
@@ -24,13 +37,18 @@ def main():
         while True:
             client, addr = sock.accept()
             print_lock.acquire()
-            print("Connected ")
-
+            print("Connected to :", addr[0])
+            sensor = threading.Thread(target = runDS, args=(threadInfo, client, ))
+            sensor.start()
+            sensorThread.append(sensor)
 
     except KeyboardInterrupt:
         ## TODO: disconnect from sensors
-        print("Core Terminated\n")
-
+        threadInfo.setRun(False)
+        eventProcessor.join()
+        for th in sensorThreads:
+            th.join()
 
 if __name__ == "__main__":
     main()
+    print("Core Terminated\n")
