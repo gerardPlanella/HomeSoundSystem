509835903fdbe752e498bda6a5d06ea720a94092
HobJob <adria.arroyo@outlook.es>
2020-04-07 18:08:24 +0200

    Reaktune

--- Sensing/audio_extraction/Realtime.m
+++ Sensing/audio_extraction/Realtime.m
@@ -6,11 +6,6 @@ ID = -1; % default audio input device
 recObj = audiorecorder(Fs,nBits,nChannels,ID);
 TimeSave = 1 ; % Time of recording in sec
 
-FilesSaved = '.\Recorded_SOUND\RealtimeSample.wav';
-dirName    = '.\Recorded_SOUND';
-path_feature_extraction = 'C:\Users\user\Documents\ecole\Master 2\Project Robotica\Extracted_SOUND';
-classLabels = {'RealtimeSample'};
-
 stWin = 100e-3;      % short-term window size (in seconds)
 stStep = 50e-3;      % short-term window step (in seconds) 
 
@@ -43,3 +38,5 @@ stFeatures = stFeatureExtraction(audioData, Fs, stWin, stStep, {'mfcc'});
 % STEP 2: mid-term feature extraction:
 [mtFeatures, st] = mtFeatureExtraction(...
     stFeatures , mtWinRatio, mtStepRatio, '');
+
+
